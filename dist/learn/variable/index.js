7c50626dea593a857766cc0a81ec7aedb58d582b
ilman <ilmanfrasetya@gmail.com>
2025-12-29 11:03:34 +0700

    feat : variable

---
+++ dist/learn/variable/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("hello world");
+let variable = "hello world";
+let numbervariable = 34;
+let booleanvariable = true;
+let arraynumber = [1, 2, 3, 4, 5, 6];
+console.log(variable, numbervariable, booleanvariable, arraynumber);
+const PHI = 3.14;
+console.log("PHI : ", PHI);
+//# sourceMappingURL=index.js.map
---
+++ src/learn/variable/index.ts
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+let variable: string = "hello world";
+let numbervariable: number = 34;
+let booleanvariable: boolean = true;
+let arraynumber: number[] = [1, 2, 3, 4, 5, 6];
+
+console.log(variable, numbervariable, booleanvariable, arraynumber);
+
+const PHI = 3.14;
+console.log("PHI : ", PHI);
